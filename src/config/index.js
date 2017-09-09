2c371a6cb87ba2995d1722ab6a146c1c56217bbf
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2017-09-09 14:45:27 +0800

    feat: split config

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+exports.source = {
+  repo: 'blog',
+  post: 'posts',
+  about: 'about/README.md',
+  owner: 'thonatos',
+}
+
+exports.github = {
+  auth: {
+    username: 'thonatos',
+    password: ['7b1d96130bd7ad7b7c96a', '6a0d6d67fdcc11b60d6'].join(''),
+  },
+}
